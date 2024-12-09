9fee30f069251ca5523155c5a57e9bb780c4b4b2
Pablo <pablo.magarinos@outlook.com>
2024-12-09 01:21:14 +0100

    Create Helloworld.f90

---
+++ 1.HelloWorld/Helloworld.f90
@@ -0,0 +1,6 @@
+program hola_mundo
+    implicit none  ! Desactiva la declaración implícita de variables
+
+    ! Imprime un mensaje en la consola
+    print *, "Hola, Mundo desde Fortran"
+end program hola_mundo
